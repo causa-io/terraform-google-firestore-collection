7d6162b4f8c5030a51b55f4a71c8f6a12874e58f
Flo <florent.vouin@gmail.com>
2023-07-25 17:50:02 +0200

    ✨ Define single_field_index_exemptions logic

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
+# This defines fields with an empty `index_config` block which disables indexing on them.
+resource "google_firestore_field" "index_exempted" {
+  for_each = var.single_field_index_exemptions
 
+  collection = var.name
+  field      = each.key
+
+  index_config {}
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,10 @@
+variable "name" {
+  type        = string
+  description = "The name of the Firestore collection."
+}
 
+variable "single_field_index_exemptions" {
+  type        = set(string)
+  description = "A set of field names that should not be indexed."
+  default     = []
+}
